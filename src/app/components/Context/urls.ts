87fd21d9c3ac5771d1e545fc8ecd6fb01b1b7d69
Jeffrey Hoekman <jeffreyscotthoekman@gmail.com>
2025-11-06 09:02:17 -0500

    Update URLs

--- src/app/components/Context/urls.ts
+++ src/app/components/Context/urls.ts
@@ -1,36 +1,7 @@
-export const urls = [{
-  url: "https://sarahokelley.substack.com/",
-  title: "Grape to Table",
-  seeded: false,
-  loading: false,
-},
-{
+export const urls = [
+  {
   url: "https://docs.google.com/document/d/1yKEcbaJUWe8Gv9qnIJgKo7k09XA_1hy7KxCw-WKF_7w/",
   title: "Jaco Dogsitting",
-  seeded: false,
-  loading: false,
-},
-{
-  url: "https://forecast.weather.gov/MapClick.php?x=273&y=107&site=chs&zmx=&zmy=&map_x=273&map_y=107",
-  title: "forecast.weather.gov - Charleston SC",
-  seeded: false,
-  loading: false,
-},
-{
-  url: "https://thehackernews.com/",
-  title: "The Hacker News",
-  seeded: false,
-  loading: false,
-},
-{
-  url: "https://phoenixnap.com/kb/linux-commands-cheat-sheet",
-  title: "Linux Commands Cheat Sheet - phoenixNAP",
-  seeded: false,
-  loading: false,
-},
-{
-  url: "https://www.windfinder.com/weatherforecast/folly_island",
-  title: "Windfinder Folly Beach",
   seeded: false,
   loading: false,
 }]
