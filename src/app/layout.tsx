daa3a107c88fccc5c089f14c5e342b972e37e927
Jeffrey Hoekman <jeffreyscotthoekman@gmail.com>
2025-11-06 22:15:50 -0500

    Update page title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: "Pinecone - Vercel AI SDK Example",
-  description: "Pinecone - Vercel AI SDK Example",
+  title: "Jaco Dog sitting Chatbot",
+  description: "Jaco Dog sitting Chatbot",
 };
 
 import "../global.css";
